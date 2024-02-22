e9fed4b6ff88f4b2b16dff0dee18b2c42e334c28
simonefrisco <simonefrisco@gmail.com>
2024-02-22 19:47:03 +0100

    fix script

--- week_4/readme.md
+++ week_4/readme.md
@@ -17,21 +17,37 @@ During this week we explore dbt.
 
 Make sure to install the dependencies:
 
+```
 pip install pandas pyarrow google-cloud-storage
 set GOOGLE_APPLICATION_CREDENTIALS=cedar-unison-413811-47e15ebf5e92.json
 set GCP_GCS_BUCKET=mage-zoomcamp-sf879
-
-python upload_dataset.py
+```
 
 Run the upload_dataset.py script
+```
+python upload_dataset.py
+```
+
 
 ## Init dbt
 
-
-
-adding this file : 
-
+setup dbt project -> https://github.com/DataTalksClub/data-engineering-zoomcamp/blob/main/04-analytics-engineering/dbt_cloud_setup.md
 
 
 # Setup DBT
 
+- make sure to install the dependencies in dbt cloud 
+
+
+packages.yaml
+```
+packages:
+  - package: dbt-labs/dbt_utils
+    version: 1.1.1
+  - package: dbt-labs/codegen
+    version: 0.12.1
+```
+
+```
+dbt deps
+```
--- week_4/upload_dataset.py
+++ week_4/upload_dataset.py
@@ -17,7 +17,7 @@ init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
 # switch out the bucketname
 BUCKET = os.environ.get("GCP_GCS_BUCKET", "mage-zoomcamp-sf879")
 
-taxi_dtypes = {
+yellow_green_dtypes = {
     'VendorID': pd.Int64Dtype(),
     'lpep_pickup_datetime': str,
     'lpep_dropoff_datetime': str,
@@ -39,7 +39,7 @@ taxi_dtypes = {
     'trip_type': pd.Int64Dtype(),
     'congestion_surcharge': float
 }
-parse_dates = ['lpep_pickup_datetime', 'lpep_dropoff_datetime']
+
 
 def upload_to_gcs(bucket, object_name, local_file):
     """
@@ -59,6 +59,22 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 
 def web_to_gcs(year, service):
+    data_dtypes = yellow_green_dtypes
+    if service == 'yellow':
+        parse_dates = ['tpep_pickup_datetime', 'tpep_dropoff_datetime']
+    elif service == 'green':
+        parse_dates = ['lpep_pickup_datetime', 'lpep_dropoff_datetime']
+    else:
+        parse_dates = ['pickup_datetime', 'dropOff_datetime']
+        data_dtypes = {
+                'dispatching_base_num': str,
+                'PUlocationID': float,
+                'DOlocationID': float,
+                'SR_Flag': float,
+                'Affiliated_base_number': str
+            }
+        
+    
     for i in range(12):
         
         # sets the month part of the file_name string
@@ -72,16 +88,17 @@ def web_to_gcs(year, service):
         # read it back into a parquet file
         try:
             print('try reading file...')
-            df = pd.read_csv(file_name, compression='gzip', low_memory=False, dtype=taxi_dtypes, parse_dates=parse_dates)
-        except:
+            df = pd.read_csv(file_name, compression='gzip', low_memory=False, dtype=data_dtypes, parse_dates=parse_dates)
+        except Exception as e :
+            print('exception : ', e)
             print('downloading file...')
             # download it using requests via a pandas df
             request_url = f"{init_url}{service}/{url_file}"
             r = requests.get(request_url)
             open(file_name, 'wb').write(r.content)
-            df = pd.read_csv(file_name, compression='gzip', low_memory=False, dtype=taxi_dtypes, parse_dates=parse_dates)
+            df = pd.read_csv(file_name, compression='gzip', low_memory=False, dtype=data_dtypes, parse_dates=parse_dates)
         
-        df['lpep_pickup_date'] = df['lpep_pickup_datetime'].dt.date
+        df['lpep_pickup_date'] = df[parse_dates[0]].dt.date
 
         print('shape : ', df.shape)
         print(f"Local: {file_name}")
@@ -119,6 +136,70 @@ def web_to_gcs(year, service):
 
 # web_to_gcs('2019', 'green')
 # web_to_gcs('2020', 'green')
-web_to_gcs('2019', 'yellow')
-web_to_gcs('2020', 'yellow')
-#web_to_gcs('2019', 'fhv')
+# web_to_gcs('2019', 'yellow')
+# web_to_gcs('2020', 'yellow')
+web_to_gcs('2019', 'fhv')
+
+
+# %%
+# generate a dict from this data:
+#  VendorID               630918 non-null  int64  
+#  1   lpep_pickup_datetime   630918 non-null  object 
+#  2   lpep_dropoff_datetime  630918 non-null  object 
+#  3   store_and_fwd_flag     630918 non-null  object 
+#  4   RatecodeID             630918 non-null  int64  
+#  5   PULocationID           630918 non-null  int64  
+#  6   DOLocationID           630918 non-null  int64  
+#  7   passenger_count        630918 non-null  int64  
+#  8   trip_distance          630918 non-null  float64
+#  9   fare_amount            630918 non-null  float64
+#  10  extra                  630918 non-null  float64
+#  11  mta_tax                630918 non-null  float64
+#  12  tip_amount             630918 non-null  float64
+#  13  tolls_amount           630918 non-null  float64
+#  14  ehail_fee              0 non-null       float64
+#  15  improvement_surcharge  630918 non-null  float64
+#  16  total_amount           630918 non-null  float64
+#  17  payment_type           630918 non-null  int64  
+#  18  trip_type              630918 non-null  int64  
+#  19  congestion_surcharge   84538 non-null   float64
+
+# data = {
+#     'VendorID': pd.Int64Dtype(),
+#     'lpep_pickup_datetime': str,
+#     'lpep_dropoff_datetime': str,
+#     'store_and_fwd_flag': str,
+#     'RatecodeID': pd.Int64Dtype(),
+#     'PULocationID': pd.Int64Dtype(),
+#     'DOLocationID': pd.Int64Dtype(),
+#     'passenger_count': pd.Int64Dtype(),
+#     'trip_distance': float,
+#     'fare_amount': float,
+#     'extra': float,
+#     'mta_tax': float,
+#     'tip_amount': float,
+#     'tolls_amount': float,
+#     'ehail_fee': float,
+#     'improvement_surcharge': float,
+#     'total_amount': float,
+#     'payment_type': pd.Int64Dtype(),
+#     'trip_type': pd.Int64Dtype(),
+#     'congestion_surcharge': float
+# }
+# # %%
+# # generate a dict from this data:
+# # 0   dispatching_base_num    1475564 non-null  object 
+# #  1   pickup_datetime         1475564 non-null  object 
+# #  2   dropOff_datetime        1475564 non-null  object 
+# #  3   PUlocationID            1474460 non-null  float64
+# #  4   DOlocationID            1474460 non-null  float64
+# #  5   SR_Flag                 0 non-null        float64
+# #  6   Affiliated_base_number  1468253 non-null  object 
+
+# data = {
+#     'dispatching_base_num': str,
+#     'PUlocationID': float,
+#     'DOlocationID': float,
+#     'SR_Flag': float,
+#     'Affiliated_base_number': str
+# }
